868d7781521180f461409cb7869c471cee25a88f
Derek Logan <75442980+delo9146@users.noreply.github.com>
2021-11-04 17:50:43 -0600

    Update main.py

--- Node/main.py
+++ Node/main.py
@@ -1,6 +1,6 @@
-import parser
 from Node import Node
 from parser import parser
+import pprint
 
 parse = parser
 
@@ -8,7 +8,30 @@ f = open("alarm.bif", "r")
 BIF = f.readlines()
 BIF = parse.fixWhiteSpace(BIF)
 nodes = parse.parseBIF(BIF)
-parse.printNodes(nodes)
 
-for node in nodes:
-    print(node)
+
+# creating a dictionary to house this info since I hate nodes
+bayesDict = {}
+# adding names of variables, nested dictionary with nested housing parents, children, states, and prob
+for a in nodes:
+    parentList = []
+    for b in a.parents:
+        parentList.append(b.getName())
+    bayesDict[a.getName()] = {'parents' : parentList}
+
+# adding children
+for a in nodes:
+    childList = []
+    for c in a.children:
+        childList.append(c.getName())
+    bayesDict[a.getName()]['children'] = childList
+
+# adding states/prob
+for a in nodes:
+    bayesDict[a.getName()]['CPD'] = a.getDist()
+
+for k,v in bayesDict.items():
+    print(k,v)
+    print()
+
+
