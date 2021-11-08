0101008a61067ff1003e403ef62c6c7a919e17ed
Derek Logan <75442980+delo9146@users.noreply.github.com>
2021-11-08 14:39:25 -0700

    Update main.py

--- Node/main.py
+++ Node/main.py
@@ -1,10 +1,18 @@
 from Node import Node
 from parser import parser
-import pprint
+from varElim import varElim
 
 parse = parser
+varE = varElim
 
 f = open("alarm.bif", "r")
+g = open("child.bif", "r")
+h = open("hailfinder.bif", "r")
+i = open("insurance.bif", "r")
+j = open("win95pts.bif", "r")
+
+bifList = [f,g,h,i,j]
+
 BIF = f.readlines()
 BIF = parse.fixWhiteSpace(BIF)
 nodes = parse.parseBIF(BIF)
@@ -34,4 +42,37 @@ for k,v in bayesDict.items():
     print(k,v)
     print()
 
+# query variables for each network
+alarmQ = ['HYPOVOLEMIA', 'LVFAILURE', 'ERRLOWOUTPUT']
+childQ = ['Disease']
+hailQ = ['SatContMoist', 'LLIQ']
+insuranceQ = ['MedCost', 'ILiCost', 'PropCost']
+winQ = ['Problem1', 'Problem2', 'Problem3', 'Problem4', 'Problem5', 'Problem6']
 
+# evidence sets for each query
+alarmEvidLittle = {'HRBP' : 'High', 'CO' : 'Low', 'BP' : 'High' }
+alarmEvidMed = {'HRBP' : 'High', 'CO' : 'Low', 'BP' : 'High', 'HRSAT' : 'Low', 'HREKG' : 'Low', 'History': 'True'}
+childEvidLittle = {'LowerBodyO2' : '<5', 'RUQO2' : '>=12', 'CO2Report' : '>=7.5', 'XrayReport' : 'AsyPatchy'}
+childEvidMed = {'LowerBodyO2' : '<5', 'RUQO2' : '>=12', 'CO2Report' : '>=7.5', 'XrayReport' : 'AsyPatchy', 'GruntingReport' : 'Yes', 'LVHReport' : 'Yes', 'Age' : '11-30 Days'}
+hailEvidLittle = {'RSFcst' : 'XNIL', 'N32StarFcst' : 'XNIL', 'MountainFcst' : 'XNIL', 'AreaMoDryAir' : 'VeryWet'}
+hailEvidMed = {'RSFcst' : 'XNIL', 'N32StarFcst' : 'XNIL', 'MountainFcst' : 'XNIL', 'AreaMoDryAir' : 'VeryWet', 'CombVerMo' : 'Down', 'AreaMeso_ALS' : 'Down', 'CurPropConv' : 'Strong'}
+insuranceEvidLittle = {'Age' : 'Adolescent', 'GoodStudent' : 'False', 'SeniorTrain' : 'False', 'DrivQuality' : 'Poor'}
+insuranceEvidMed = {'Age' : 'Adolescent', 'GoodStudent' : 'False', 'SeniorTrain' : 'False', 'DrivQuality' : 'Poor', 'MakeModel' : 'Luxury', 'CarValue' : 'FiftyThousand', 'DrivHistory' : 'Zero'}
+winEvidOne = {'Problem1' : 'No_OutPut'}
+winEvidTwo = {'Problem2' : 'Too_Long'}
+winEvidThree = {'Problem3' : 'No'}
+winEvidFour = {'Problem4' : 'No'}
+winEvidFive = {'Problem5' : 'No'}
+winEvidSix = {'Problem6' : 'Yest'}
+winEvid = [winEvidOne, winEvidTwo, winEvidThree, winEvidFour, winEvidFive, winEvidSix]
+insuarnceEvid = [insuranceEvidLittle, insuranceEvidMed]
+hailEvid = [hailEvidLittle, hailEvidMed]
+childEvid = [childEvidLittle, childEvidMed]
+alarmEvid = [alarmEvidLittle, alarmEvidMed]
+
+
+
+# alarm little
+for i in alarmQ:
+    for j in alarmEvid:
+        varE.query(bayesDict,j, i)
